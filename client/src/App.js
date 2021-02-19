c8396e4ae7067ab3dc4dddb047c9096731aab9f2
gaacasseb <ogabrielcasseb@gmail.com>
2021-02-19 18:31:50 -0300

    Express e Node adicionado

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a>Entrou</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
