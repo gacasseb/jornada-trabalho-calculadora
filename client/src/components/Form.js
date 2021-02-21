91f240cabb10eab5996837580ebe3f576f7028fb
gaacasseb <ogabrielcasseb@gmail.com>
2021-02-21 20:07:51 -0300

    VET-05 renderizando os valores

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -8,17 +8,32 @@ function Form(props) {
 
     var [ time, setTime ] = useState({})
     var [ loading, setLoading ] = useState(false)
+    const [ result, setResult ] = useState({})
 
     const fetch = () => {
         setLoading(true)
         axios.post('http://localhost:4000/count-hours', time)
         .then((response) => {
             setLoading(false)
-            console.log(response.data)
+            console.log(response)
+            if ( response.status == 200 ) {
+                let data = response.data
+                setResult({
+                    dayTime: {
+                        hour: data.dayTime.hour,
+                        minute: data.dayTime.minute
+                    },
+                    nocturnalTime: {
+                        hour: data.nocturnalTime.hour,
+                        minute: data.nocturnalTime.minute
+                    }
+                })
+            }
         })
         .catch(error => {
             setLoading(false)
             console.log(error)
+            alert(error)
         })
     }
 
@@ -43,6 +58,24 @@ function Form(props) {
         if ( loading ) {
             return (
                 <ProgressBar />
+            )
+        }
+    }
+
+    const renderResults = () => {
+        if ( result.dayTime && result.nocturnalTime ) {
+            return (
+                <div>
+                    <Autocomplete title='Período diurno' value={`${result.dayTime.hour} hora(s) e ${result.dayTime.minute} minuto(s)`} disabled></Autocomplete>
+                    <Autocomplete title='Período noturno' value={`${result.nocturnalTime.hour} hora(s) e ${result.nocturnalTime.minute} minuto(s)`} disabled></Autocomplete>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <Autocomplete title='Período diurno' value={`00 : 00`} disabled></Autocomplete>
+                    <Autocomplete title='Período noturno' value={`00 : 00`} disabled></Autocomplete>
+                </div>
             )
         }
     }
@@ -87,8 +120,9 @@ function Form(props) {
                 <Tab title='Resultados' className='tab-content'>
                     <div style={{display: 'flex', justifyContent: 'center'}}>
                         <Row>
-                            <Autocomplete title='Período diurno' disabled></Autocomplete>
-                            <Autocomplete title='Período noturno' disabled></Autocomplete>
+                            {renderResults()}
+                            {/* <Autocomplete title='Período diurno' value={`${result.dayTime.hour} : ${result.dayTime.minute}`} disabled></Autocomplete>
+                            <Autocomplete title='Período noturno' disabled></Autocomplete> */}
                         </Row>
                     </div>
                 </Tab>
